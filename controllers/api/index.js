14b10b31b18e1338d4462dbaae3ee011dfffbf7d
Mizuri <pro.1.h@outlook.com>
2022-10-26 21:15:57 -0600

    updated index

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,21 @@
-const router = require('express').Router();
-const userRoutes = require('./userRoutes');
-const projectRoutes = require('./projectRoutes');
+//An index file to gather the API routes and export them for use
 
+//Dependencies
+//Server connection
+const router = require('express').Router();
+//User routes
+const userRoutes = require('./userRoutes');
+//Post routes
+const postRoutes = require('./post-routes');
+//Project routes
+const projectRoutes = require('./projectRoutes');
+//Comment routes
+const commentRoutes = require('./comment-routes');
+
+//Define route path for the API to use
 router.use('/users', userRoutes);
 router.use('/projects', projectRoutes);
+router.use('/posts', postRoutes);
+router.use('/comments', commentRoutes);
 
 module.exports = router;
