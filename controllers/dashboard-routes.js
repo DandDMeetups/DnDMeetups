149ed4d4e07d628f2a7c092c8b59be8e02115d20
Mizuri <pro.1.h@outlook.com>
2022-10-26 21:00:18 -0600

    added more code in dashboard routes

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -36,9 +36,12 @@ router.get('/', withAuth, (req, res) => {
             }
         ]
     })
-    .then() => {
-        
-    }).catch((err) => {
+    .then(dbPostData => {
+        //Serialize data before passing to template
+        const posts = dbPostData.map(post => post.get({ plain: true }));
+        res.render('dashboard', { posts, logged_in: true });
+         })
+    .catch((err) => {
         console.log(err);
         res.status(500).json(err);
         
@@ -46,7 +49,7 @@ router.get('/', withAuth, (req, res) => {
 });
 
 //Route to edit a post
-router.get('', withAuth, (req, res) => {
+router.get('/edit/:id', withAuth, (req, res) => {
     //All of the users posts are obtained from the database
     Post.fineOne({
         where: {
@@ -73,7 +76,48 @@ router.get('', withAuth, (req, res) => {
             }
         ]
     })
-    .then( => {
-        
+    .then(dbPostData => {
+        //If no post by that id exists, return an error
+        if(!dbPostData) {
+            res.status(404).json({ message: 'No post found with this id' });
+            return;
+        }
+        //Serialize data before passing to template
+        const post = dbPostData.get({ plain: true });
+        res.render('edit-post,' { post, logged_in: true });
     })
-})
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+//Route to edit the logged in user
+router.get('/edituser', withAuth, (req, res) => {
+    //Access the User model and run the fineOne() method to get a single user based on the parameters
+    User.findOne({
+        //When the data is sent back, exclude the password property
+        attributes: { exclude: ['password'] },
+        where: {
+            //Use id as the parameter for the request
+            id: req.session.user_id
+        }
+    })
+    .then(dbUserData => {
+        if(!dbUserData) {
+            //If no user is found, return an error
+            res.status(404).json({ message: 'No user found with this id' });
+            return;
+        }
+        //Otherwise, return the data for the requested user
+        const user = dbUserData.get({ plain: true });
+        res.render('edit-user', { user, logged_in: true });
+    })
+    .catch(err => {
+        //If there is a server error, return that error
+        console.log(err);
+        res.status(500).json(err);
+    })
+});
+
+module.exports = router; 
